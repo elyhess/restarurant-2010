39afd8e555740eb2c59d90fa3a7e66644556fa4e
Ely Hess <ely.hess@me.com>
2020-10-07 13:51:26 -0600

    Add announce_closing_time test

--- test/restaurant_test.rb
+++ test/restaurant_test.rb
@@ -68,12 +68,15 @@ class RestaurantTest < Minitest::Test
     restaurant2.add_dish('Ravioli')
 
     assert_equal ["BURRATA", "PIZZETTA", "RAVIOLI"], restaurant2.menu_dish_names
-
   end
 
+  def test_announce_closing_time
+    restaurant1 = Restaurant.new('6:00', 'Fuel Cafe')
+    restaurant2 = Restaurant.new('16:00', 'Il Posto')
 
-
-
+    assert_equal "Fuel Cafe will be closing at 11:00AM", restaurant1.announce_closing_time(5)
+    assert_equal "Il Posto will be closing at 11:00PM", restaurant2.restaurant2.announce_closing_time(7)
+  end
 
 
 end
